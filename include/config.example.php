2867251f964ff4afbbe601770c68c9f8a629a5b9
oO.para@gmail.com <oO.para@gmail.com@b22b2325-a61c-fcad-14ea-62432523ddbd>
2015-02-03 23:51:57 +0000

    Bootstrap and Example Config file

---
+++ include/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+define('MKM_APP_TOKEN', '');
+define('MKM_APP_SECRET', '');
+define('MKM_ACCESS_TOKEN', '');
+define('MKM_ACCESS_TOKEN_SECRET', '');
+
+define('SITEURL', 'http://localhost/mkmcalc/www');
