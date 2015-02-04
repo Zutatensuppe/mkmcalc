416f79926d40f6d21218dc420157914c48cb85f0
oO.para@gmail.com <oO.para@gmail.com@b22b2325-a61c-fcad-14ea-62432523ddbd>
2015-02-04 00:10:23 +0000

    db config is in config file now

--- include/config.example.php
+++ include/config.example.php
@@ -5,4 +5,9 @@ define('MKM_APP_SECRET', '');
 define('MKM_ACCESS_TOKEN', '');
 define('MKM_ACCESS_TOKEN_SECRET', '');
 
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
 define('SITEURL', 'http://localhost/mkmcalc/www');
